84cd6c664672dfa2b56c6126eb55a35d2f1040cf
oilcode <oilcode@d3d76d48-7f73-481b-a58f-5f97c03e34ac>
2013-10-03 14:06:59 +0000

    增加了tolua++

---
+++ trunk/C++AndLua/LuaExportSample.h
@@ -0,0 +1,9 @@
+#ifndef _LuaExportSample_h_
+#define _LuaExportSample_h_
+
+namespace LuaExport
+{
+	void Script_GetNumber(int& nFirst, int& nSecond);
+}
+
+#endif
--- trunk/C++AndLua/Test.cpp
+++ trunk/C++AndLua/Test.cpp
@@ -1,10 +1,15 @@
 ﻿#include "SoLua.h"
 using namespace GGUI;
+extern int  tolua_toluapp_open (lua_State* tolua_S);
 
 void main()
 {
 	lua_State* L = lua_open();
 	luaL_openlibs(L);
+	tolua_toluapp_open(L);
+	/* 运行LUA脚本 */
+	luaL_dofile(L, "test.lua");
+
 	SoLua::ExecuteFile(L, "add.lua");
 	int nExeResult = 0;
 	SoLua::ExecuteFunc(L, "add", "ii>i", 3, 4, &nExeResult);
